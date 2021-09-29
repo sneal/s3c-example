d8e3fc3f812e862f464f86048ef777b7c84db0f9
Shawn Neal <sneal@sneal.net>
2021-09-28 22:00:08 -0700

    Add default shell via env

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env sh
+
+set -eux
+
 cp s3c-repo/ci/harbor.crt /usr/local/share/ca-certificates/harbor.crt
 cat /usr/local/share/ca-certificates/harbor.crt >> /etc/ssl/certs/ca-certificates.crt
 

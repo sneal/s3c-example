942bf70a299983b1666f8111e4b6e729a509d896
Shawn Neal <sneal@sneal.net>
2021-09-28 21:55:21 -0700

    Remove shebang
    Troubleshooting "tarting container process caused: no such file or directory"

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,7 +1,3 @@
-#!/usr/bin/sh
-
-set -eux
-
 cp s3c-repo/ci/harbor.crt /usr/local/share/ca-certificates/harbor.crt
 cat /usr/local/share/ca-certificates/harbor.crt >> /etc/ssl/certs/ca-certificates.crt
 

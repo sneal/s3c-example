19aa6e1547f0d27e9bec9c9abbec82ff387b210f
Shawn Neal <sneal@sneal.net>
2021-09-29 21:56:47 -0700

    Add image dir creation

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -5,7 +5,12 @@ set -eux
 cp s3c-repo/ci/harbor.crt /usr/local/share/ca-certificates/harbor.crt
 cat /usr/local/share/ca-certificates/harbor.crt >> /etc/ssl/certs/ca-certificates.crt
 
+# the build task below requires an output dir of image
+mkdir -p image
+
+# build the container image using the oci builder task
 build
 
+# remap the image dir to s3c-image
 mkdir -p s3c-image
 cp image/* s3c-image/

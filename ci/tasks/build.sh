a8c0c3f5649424787810a87296b5434d25e230c2
Shawn Neal <sneal@sneal.net>
2021-09-29 20:39:01 -0700

    Add s3c-image as output to build

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -6,3 +6,5 @@ cp s3c-repo/ci/harbor.crt /usr/local/share/ca-certificates/harbor.crt
 cat /usr/local/share/ca-certificates/harbor.crt >> /etc/ssl/certs/ca-certificates.crt
 
 build
+
+mv ../image/image.tar ./s3c-image/image.tar
